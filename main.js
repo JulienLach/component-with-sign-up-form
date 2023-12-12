0410cc9c7ff5d2e4563e1e81c54c672fdb0ee9a2
JulienLach <leadedchamps@gmail.com>
2023-12-12 11:33:46 +0100

    ok

--- main.js
+++ main.js
@@ -9,30 +9,39 @@ document.getElementById('submit-btn').addEventListener('click', function (event)
     const lastNameError = document.querySelector('.last-name-error');
     const emailError = document.querySelector('.email-error');
     const passwordError = document.querySelector('.password-error');
+    const firstNameIconError = document.querySelector('.first-name-icon-error')
+    const lastNameIconError = document.querySelector('.last-name-icon-error')
+    const passwordIconError = document.querySelector('.password-icon-error')
+    const emailIconError = document.querySelector('.email-icon-error')
+
 
     if (firstName === '') {
         firstNameError.classList.add('active')
         document.querySelector('.first-name').style.border = '2px solid hwb(0 48% 0%)';
         document.querySelector('.first-name').classList.add('placeholder-error');
-
+        firstNameIconError.classList.add('active')
     }
 
     if (lastName === '') {
         lastNameError.classList.add('active')
         document.querySelector('.last-name').style.border = '2px solid hwb(0 48% 0%)';
         document.querySelector('.last-name').classList.add('placeholder-error');
+        lastNameIconError.classList.add('active')
+
     }
 
     if (email === '') {
         emailError.classList.add('active')
         document.querySelector('.email').style.border = '2px solid hwb(0 48% 0%)';
         document.querySelector('.email').classList.add('placeholder-error');
+        emailIconError.classList.add('active')
     }
 
     if (password === '') {
         passwordError.classList.add('active')
         document.querySelector('.password').style.border = '2px solid hwb(0 48% 0%)';
         document.querySelector('.password').classList.add('placeholder-error');
+        passwordIconError.classList.add('active')
     }
 
 
